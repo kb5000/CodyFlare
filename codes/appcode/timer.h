deb742dced5499839a8a04c066ba7c23db965782
MZR <3180105698@zju.edu.cn>
2019-03-26 22:29:00 +0800

    bug fix

--- codes/appcode/timer.h
+++ codes/appcode/timer.h
@@ -1,12 +1,2 @@
 #pragma once
-#include <time.h>
-#include <minwindef.h>
-#include <WinBase.h>
 
-typedef struct _My_Timer {
-	DWORD startTime;
-	DWORD tickPeroid;
-	void (*start)(struct _My_Timer* self);
-	void (*stop)(struct _My_Timer* self);
-	void (*func_when_tick)(void* paras);
-} Timer;
