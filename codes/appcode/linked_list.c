d90e6e5a9ee6fec061a29a63990ed5c9fc8db92a
MZR <3180105698@zju.edu.cn>
2019-04-03 07:23:03 +0800

    advanced sort

--- codes/appcode/linked_list.c
+++ codes/appcode/linked_list.c
@@ -184,18 +184,21 @@ Node* my_list_sort_merge(Node* lhs, Node* rhs, int func(void*, void*)) {
 
 Node* sort_list(Node* head, int func(void*, void*)) {
 	if (!head || !head->next) return head;
-	Node* lastMid = head;
+	//Node* lastMid = head;
 	Node* mid = head;
 	Node* tail = head->next;
 	while (1) {
-		lastMid = mid;
+		//lastMid = mid;
+		tail = tail->next;
+		if (!tail) break;
 		mid = mid->next;
 		tail = tail->next;
 		if (!tail) break;
-		tail = tail->next;
-		if (!tail) break;
 	}
-	lastMid->next = NULL;
+	//lastMid->next = NULL;
+	tail = mid;
+	mid = mid->next;
+	tail->next = NULL;
 	Node* hd = sort_list(head, func);
 	Node* md = sort_list(mid, func);
 	return my_list_sort_merge(hd, md, func);
