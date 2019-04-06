9e508bf969db4de08522cae3eacbf3789aac48f4
MZR <3180105698@zju.edu.cn>
2019-04-06 11:20:31 +0800

    test of ui

--- codes/appcode/main_test.c
+++ codes/appcode/main_test.c
@@ -2,7 +2,7 @@
 
 void tests() {
 	//InitConsole();
-	////test_of_vector();
+	//test_of_vector();
 	//test_of_list();
 	//test_of_timer();
 	//test_of_function();
--- codes/appcode/main_test.h
+++ codes/appcode/main_test.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "graphics.h"
+
 #include "vector_test.h"
 #include "list_test.h"
 #include "timer_test.h"
 #include "function_test.h"
-#include "graphics.h"
 #include "anime_test.h"
+#include "collision_test.h"
+#include "ui_test.h"
 
 void tests();
