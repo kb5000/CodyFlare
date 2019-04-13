7d51fb0e3b8f356db1809b2b8a26b495aaab6c46
MZR <3180105698@zju.edu.cn>
2019-04-13 23:03:45 +0800

    listbox

---
+++ codes/appcode/listbox.c
@@ -0,0 +1,5 @@
+#include "listbox.h"
+
+Node* show_list_box(int id, ListHandler list, Pos position, int currentItem) {
+
+}
---
+++ codes/appcode/listbox.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "linked_list.h"
+#include "utility.h"
+
+Node* show_list_box(int id, ListHandler list, Pos position, int currentItem);
