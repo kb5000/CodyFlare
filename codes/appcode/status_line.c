ad73d40ca7bcb51c7b3f94c8d98fff53b9aaa828
wjss <a123edcrfv@live.cn>
2019-05-20 16:52:04 +0800

    Update status_line.c

--- codes/appcode/status_line.c
+++ codes/appcode/status_line.c
@@ -5,16 +5,20 @@
 #include <stdlib.h>
 
 void show_status_line(int score, int hp, int bombs) {
+    hnew(Vector, vec);
+    *vec = gen_empty_vector(Vector);
+    Vector v = gen_empty_vector(char*);
 	char* t[3] = {malloc(24), malloc(24), malloc(24)};
 	sprintf(t[0], "score: %d", score);
 	sprintf(t[1], "health: %d", hp);
 	sprintf(t[2], "bombs: %d", bombs);
-	hnew(Vector, v);
-	*v = gen_empty_vector(char*);
+	//hnew(Vector, v);
+	//*v = gen_empty_vector(char*);
 	pcalls(v, push, t[0]);
 	pcalls(v, push, t[1]);
 	pcalls(v, push, t[2]);
+    pcalls(vec, push, &v);
 	//Vector v = 
-	show_sheet(v, new_pos(0, 0), new_pos(10, 0.15), 1, 3, 0, 1, 0, color_by_name("DarkGrey"),
-			   color_by_name("Red"), color_by_rgb(210, 210, 210), "ËÎÌå", 1, "Î¢ÈíÑÅºÚ", 14);
+	show_sheet(vec, new_pos(0, 0), new_pos(10, 0.15), 1, 3, 0, 1, 0, color_by_name("DarkGrey"),
+			   color_by_name("Red"), color_by_rgb(210, 210, 210), "Ã‹ÃŽÃŒÃ¥", 1, "ÃŽÂ¢ÃˆÃ­Ã‘Ã…ÂºÃš", 14);
 }
