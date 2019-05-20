5e0a33fa4cbc413699977d802545fd92eb4c03b0
MZR <3180105698@zju.edu.cn>
2019-05-20 20:44:12 +0800

    cc

--- codes/appcode/status_line.c
+++ codes/appcode/status_line.c
@@ -7,18 +7,19 @@
 void show_status_line(int score, int hp, int bombs) {
     hnew(Vector, vec);
     *vec = gen_empty_vector(Vector);
-    Vector v = gen_empty_vector(char*);
-	char* t[3] = {malloc(24), malloc(24), malloc(24)};
-	sprintf(t[0], "score: %d", score);
-	sprintf(t[1], "health: %d", hp);
-	sprintf(t[2], "bombs: %d", bombs);
-	//hnew(Vector, v);
-	//*v = gen_empty_vector(char*);
-	pcalls(v, push, t[0]);
-	pcalls(v, push, t[1]);
-	pcalls(v, push, t[2]);
+    Vector v = gen_empty_vector(24);
+	char empty[24];
+	calls(v, push, empty);
+	calls(v, push, empty);
+	calls(v, push, empty);
+	sprintf(calls(v, at, 0) , "score: %d", score);
+	sprintf(calls(v, at, 1), "health: %d", hp);
+	sprintf(calls(v, at, 2), "bombs: %d", bombs);
     pcalls(vec, push, &v);
 	//Vector v = 
 	show_sheet(vec, new_pos(0, 0), new_pos(10, 0.15), 1, 3, 0, 1, 0, color_by_name("DarkGrey"),
-			   color_by_name("Red"), color_by_rgb(210, 210, 210), "ËÎÌå", 1, "Î¢ÈíÑÅºÚ", 14);
+			   color_by_name("Red"), color_by_rgb(210, 210, 210), "宋体", 1, "微软雅黑", 14);
+	calls(v, destroy);
+	pcalls(vec, destroy);
+	//free(vec);
 }
