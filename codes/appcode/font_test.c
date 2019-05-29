5b3641df9c60312ddfc87b39bc6676a7bb924c51
bfy.5@163.com <bfy.5@163.com>
2019-05-29 21:27:21 +0800

    .

--- codes/appcode/font_test.c
+++ codes/appcode/font_test.c
@@ -15,12 +15,27 @@ void test_of_font() {
 	double W = GetWindowWidth();
 	double h = size * H / 20.0;//×ÖÌå¿ò¸ß
 	double w = size * W / 40.0;//×ÖÌå¿ò¿í
-	char title[20] = {"PAUSE"};
-	int len = strlen(title);
+	char title[10][20] = { "TO THE SPACE","START","VICTORY","GAME OVER","PAUSE" };
+	char letters[20];
+
+	strcpy(letters, title[4]);//title[i]
+
+	if (!strcmp(letters, "TO THE SPACE"))
+		letter_color = 1;
+	else if (!strcmp(letters, "START"))
+		letter_color = 2;
+	else if (!strcmp(letters, "VICTORY"))
+		letter_color = 3;
+	else if (!strcmp(letters, "GAME OVER"))
+		letter_color = 4;
+	else if (!strcmp(letters, "PAUSE"))
+		letter_color = 5;
+
+	int len = strlen(letters);
 	double xStart = W / 2.0 - w * len / 2.0;
 	for (i = 0; i < len; i++) {
-		if (title[i] == ' ')continue;
-		ShowLetter(title[i], size, xStart + i * w, (H + 1.7*h) / 2);
+		if (letters[i] == ' ')continue;
+		ShowLetter(letters[i], size, xStart + i * w, (H + 1.7*h) / 2, letter_color);
 	}
 
 }
